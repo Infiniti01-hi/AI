d7f4701e3ff180599116023e06601c34edd1ae3d
Infiniti01-hi <eldarmuldagaliev0@gmail.com>
2026-07-26 09:51:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,5 +37,5 @@ def send_welcome(message):
 
 # Запуск Flask-сервера на порту, который требует Render
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
+  port = int(os.environ.get("PORT", 10000))
   app.run(host="0.0.0.0", port=port)
